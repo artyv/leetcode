781b676e18ae35c4127dedf29341b40bd6208b84
Arty <arty@Arty-MacBook.local>
2025-05-17 17:24:51 +0200

    Daily challenge

--- Sort Colors/solution_2.py
+++ Sort Colors/solution_2.py
@@ -3,17 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i0 = 0; i1 = nums.count(0); i2 = len(nums)-1; 
-        nums_copy = [-1] * len(nums)
-
-        for x in nums:
-            if x == 0:
-                nums_copy[i0] = 0
-                i0 += 1
-            elif x == 1:
-                nums_copy[i1] = 1
-                i1 += 1
-            else:
-                nums_copy[i2] = 2
-                i2 -= 1
-        nums[:] = nums_copy
+        def insertion_sort(my_list):
+            for i in range(1, len(my_list)):
+                j = i - 1
+                while j >= 0 and my_list[j+1] < my_list[j]:
+                    my_list[j+1], my_list[j] = my_list[j], my_list[j+1]
+                    j -= 1
+            return my_list
+        
+        insertion_sort(nums)
